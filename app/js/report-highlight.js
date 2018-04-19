a0fbc9d33bbf201a4cd6eacc2b9d991e513e67d2
Gaurav Trivedi <trivedigaurav@gmail.com>
2018-04-19 13:52:40 -0400

    Remove text condition

--- app/js/report-highlight.js
+++ app/js/report-highlight.js
@@ -1,7 +1,7 @@
 jQuery.fn.highlight = function (regex, className, scrollTo) {
     return this.each(function () {
         $(this).contents().filter(function() {
-            return this.nodeType == 3 && regex.test(this.nodeValue);
+            return regex.test(this.nodeValue);
         }).replaceWith(function() {
             return (this.nodeValue || "").replace(regex, function(match) {
             	if(scrollTo)
