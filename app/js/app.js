8bde459fef25e08a6116989bc5a1d73c6b10e0d8
Gaurav Trivedi <trivedigaurav@gmail.com>
2019-01-28 17:22:00 -0500

    Revert "Add basic auth"
    This reverts commit 2e52ff15af639b88830805b29f846d7965626de2.

--- app/js/app.js
+++ app/js/app.js
@@ -16,10 +16,3 @@ var app = angular.module('myApp', [
   'pagemap.directive',
   'feedbackmenu'
 ]);
-
-
-app.run(['$cookieStore', '$http',
-    function ($cookieStore, $http) {
-        // keep user logged in after page refresh
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + $cookieStore.get('authdata');
-    }]);
