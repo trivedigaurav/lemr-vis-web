55276b02b709eb8b05d4968072a4b36b39c22686
Gaurav Trivedi <trivedigaurav@gmail.com>
2019-02-01 13:29:00 -0500

    Read encounters from a json

--- app/js/services.js
+++ app/js/services.js
@@ -109,6 +109,12 @@ angular.module('myApp.services', [])
                         }
             );
         },
+
+        getEncounterList: function(){
+            return $http.get('encounter-select/encounters.json').then(function(result) {
+                return result.data;
+            });
+        }
     };
 }])
 
